056b434a83f9c9352a4740067aa1cf5942ac5ea3
Sandeep Nayak <>
2024-03-03 18:23:05 +0530

    feat: Tab view

--- music_player_app/lib/widgets/home_page.dart
+++ music_player_app/lib/widgets/home_page.dart
@@ -1,31 +1,30 @@
-import 'dart:typed_data';
-
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:provider/provider.dart';
-
 import '../common/models/audio_model.dart';
 import '../common/models/music_settings_model.dart';
 import '../common/models/shared_data_model.dart';
-import '../common/widgets/selected_song_view.dart';
-import '../common/widgets/song_list_view.dart';
-import 'playing_song_screen.dart';
+import '../common/models/side_bar_model.dart';
+import '../common/widgets/song_list_screen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
     required this.storageAccess,
+    required this.songList,
     super.key,
   });
+
   final bool storageAccess;
+  final List<SongModel> songList;
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
   bool fileAccess = false;
   final OnAudioQuery audioQuery = OnAudioQuery();
   LocalSavingDataModel localSavingDataModel = LocalSavingDataModel();
@@ -37,36 +36,20 @@ class _HomePageState extends State<HomePage> {
   SongModel? selectedSong;
   bool selectedSongLiked = false;
   bool tapOnSelectedSong = false;
+  late TabController _tabController;
 
   @override
   void initState() {
     super.initState();
     fileAccess = widget.storageAccess;
-    if (fileAccess) {
-      fetchAudioFiles();
-    }
-    updateSelectedSongFromLocalStorage();
+    songs = widget.songList;
+    _tabController = TabController(length: SideBar.values.length, vsync: this);
   }
 
-  Future<void> fetchAudioFiles() async {
-    MusicSettings.instance.fetchAudioFiles().then(
-          (value) => setState(
-            () {
-              songs = value;
-            },
-          ),
-        );
-  }
-
-  Future<void> updateSelectedSongFromLocalStorage() async {
-    final song = await localSavingDataModel.getCurrentPlayingSong();
-    if (song != null) {
-      final isLiked = await localSavingDataModel.checkForLikedSong(song);
-      setState(() {
-        selectedSong = song;
-        selectedSongLiked = isLiked;
-      });
-    }
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   Future<void> requestPermissions() async {
@@ -83,7 +66,7 @@ class _HomePageState extends State<HomePage> {
     if (statuses[Permission.audio] == PermissionStatus.granted ||
         statuses[Permission.storage] == PermissionStatus.granted) {
       logger.d('Storage permission granted');
-      await fetchAudioFiles();
+      songs = await MusicSettings.instance.fetchAudioFiles();
       setState(() {
         fileAccess = true;
       });
@@ -92,164 +75,64 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  Future<Uint8List?> getArtwork(int songId) async {
-    try {
-      final Uint8List? artwork =
-          await audioQuery.queryArtwork(songId, ArtworkType.AUDIO, size: 200);
-      return artwork;
-    } catch (e) {
-      logger.e('Error fetching artwork: $e');
-      return null;
-    }
-  }
+  late final requestPermissionScreen = Center(
+    child: TextButton(
+      onPressed: () async {
+        await requestPermissions();
+      },
+      child: const Text('Allow'),
+    ),
+  );
+
+  late final songListScreeen = SongListScreen(
+    selectedSong: selectedSong,
+    songs: songs,
+  );
+
+  late final likedSongsScreen = const Center(
+    child: Text('Liked Songs'),
+  );
 
   @override
   Widget build(BuildContext context) {
-    final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-
-    late final requestPermissionScreen = MaterialPage(
-      child: Center(
-        child: TextButton(
-          onPressed: () async {
-            await requestPermissions();
-          },
-          child: const Text('Allow'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Music Player'),
+        bottom: TabBar(
+          controller: _tabController,
+          isScrollable: true,
+          tabs: fileAccess
+              ? [
+                  for (SideBar item in SideBar.values)
+                    Tab(
+                      text: item.getFieldName(),
+                    )
+                ]
+              : [],
         ),
       ),
-    );
-
-    late final songListScreeen = MaterialPage(
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Text('Music Player'),
-        ),
-        body: Stack(
-          children: [
-            ListView.separated(
-              itemBuilder: (BuildContext context, int index) {
-                return SongListView(
-                  title: songs[index].title,
-                  artist: songs[index].artist ?? 'Unknown',
-                  onTap: () async {
-                    audioPlayerModel.currentSong = songs[index];
-                    setState(() {
-                      selectedSong = songs[index];
-                    });
-                  },
-                  leadingIcon: FutureBuilder(
-                    future: getArtwork(songs[index].id),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.done &&
-                          snapshot.data != null) {
-                        return CircleAvatar(
-                          backgroundImage:
-                              MemoryImage(snapshot.data as Uint8List),
-                        );
-                      } else {
-                        return const Icon(Icons.music_note);
-                      }
-                    },
-                  ),
-                );
-              },
-              separatorBuilder: (BuildContext context, int index) =>
-                  const Divider(
-                thickness: 0.1,
-                color: Colors.grey,
-              ),
-              itemCount: songs.length,
-            ),
-            // Overlay Widget
-            if (selectedSong != null)
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  padding: const EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(
-                      color: Colors.grey,
-                    ),
-                    boxShadow: const [
-                      BoxShadow(
-                        spreadRadius: 2,
-                        blurRadius: 1,
-                        offset: Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  child: Consumer<AudioPlayerModel>(
-                    builder: (context, audioModel, child) {
-                      return SelectedSongView(
-                        artist: selectedSong?.artist ?? 'Unknown',
-                        title: selectedSong?.title ?? 'Unknown',
-                        onTap: () {
-                          showModalBottomSheet<SongModel>(
-                            isScrollControlled: true,
-                            context: context,
-                            builder: (context) {
-                              return PlayingSongScreen(
-                                selectedSong: selectedSong,
-                                songList: songs,
-                              );
-                            },
-                          ).then((currentSong) {
-                            setState(() {
-                              selectedSong = MusicSettings.selectedSong;
-                            });
-                          });
-                        },
-                        trailingIcon: IconButton(
-                          onPressed: () async {
-                            final isLiked = await localSavingDataModel
-                                .addLikedSong(selectedSong!);
-                            if (isLiked) {
-                              setState(() {
-                                songs.add(selectedSong!);
-                              });
-                            }
-                          },
-                          icon: selectedSongLiked
-                              ? const Icon(Icons.favorite)
-                              : const Icon(Icons.favorite),
-                        ),
-                        leadingIcon: FutureBuilder(
-                          future: getArtwork(selectedSong?.id ?? 0),
-                          builder: (context, snapshot) {
-                            if (snapshot.connectionState ==
-                                    ConnectionState.done &&
-                                snapshot.data != null) {
-                              return CircleAvatar(
-                                backgroundImage:
-                                    MemoryImage(snapshot.data as Uint8List),
-                              );
-                            } else {
-                              return const CircleAvatar(
-                                backgroundColor: Colors.grey,
-                              );
-                            }
-                          },
-                        ),
-                      );
-                    },
-                  ),
+      body: fileAccess
+          ? TabBarView(
+              controller: _tabController,
+              children: [
+                songListScreeen,
+                likedSongsScreen,
+                const Center(
+                  child: Text('Artists'),
                 ),
-              ),
-          ],
-        ),
-      ),
-    );
-
-    return Navigator(
-      key: navigatorKey,
-      onPopPage: (route, result) => route.didPop(result),
-      pages: [
-        songListScreeen,
-        if (fileAccess == false) requestPermissionScreen,
-      ],
+                const Center(
+                  child: Text('Albums'),
+                ),
+                const Center(
+                  child: Text('Playlists'),
+                ),
+                const Center(
+                  child: Text('none'),
+                ),
+              ],
+            )
+          : requestPermissionScreen,
     );
   }
 }
