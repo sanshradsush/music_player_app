bd3b43ddc90abcefe1ea85178285407e3bdab01f
Sandeep Nayak <>
2024-03-08 01:15:40 +0530

    feat: Tab bar screens implementation

---
+++ music_player_app/lib/common/widgets/album_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:on_audio_query/on_audio_query.dart';
+import '../models/music_settings_model.dart';
+
+class AlbumScreen extends StatefulWidget {
+  const AlbumScreen({
+    super.key,
+  });
+
+  @override
+  State<AlbumScreen> createState() => _AlbumScreenState();
+}
+
+class _AlbumScreenState extends State<AlbumScreen> {
+  MusicSettings musicSettings = MusicSettings.instance;
+  List<AlbumModel> albums = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchAlbums();
+  }
+
+  Future<void> fetchAlbums() async {
+    final fetchedPlaylists = await musicSettings.fetchAlbums();
+    setState(() {
+      albums = fetchedPlaylists;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Albums(${albums.length})',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          const SizedBox(height: 20),
+          Expanded(
+            child: ListView.builder(
+              itemCount: albums.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: const CircleAvatar(
+                    child: Icon(Icons.person),
+                  ),
+                  title: Text(albums[index].album),
+                  subtitle: Text('${albums[index].artist}'),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
